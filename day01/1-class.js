1b99762891c83fe5347c99a530d603c86dd62e0f
Dylan <botao@babyeye.com>
2018-01-24 22:37:13 +0800

    get

---
+++ day01/1-class.js
@@ -0,0 +1,11 @@
+const http = require('http')
+const urlLib = require('url')
+
+const server = http.createServer((req, res) => {
+  const {pathname, query} = urlLib.parse(req.url, true)
+  console.log(pathname, query)
+  res.write('hello')
+  res.end()
+})
+
+server.listen('8080')
